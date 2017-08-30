2dd5a4533ad0fb54a10f4b32601f21631b8367c3
Peter Martinson <peter.j.martinson@gmail.com>
2017-08-29 21:09:23 -0400

    Add Mocha in browser
    1. To run tests, point browser to `http://localhost:3000/test`
    2. Haven't figured out how to include an assertion library.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname);
 });
 
+app.get('/test', function(req, res) {
+  res.sendFile(__dirname + '/src/test/test.html');
+});
+
 app.listen(3000, function() {
   console.log('Vanilla Blog listening on port 3000');
 });
