1a99f160d029a976f823dafb6729235afbc0b080
Peter Martinson <peter.j.martinson@gmail.com>
2017-08-30 07:57:23 -0400

    Add a kind-of working test
    I'm not sure what exactly is `article` that I'm testing!  It's an
    object, but the text appears in several properties...

--- src/test/test.js
+++ src/test/test.js
@@ -5,3 +5,10 @@ describe('the test harness', function() {
     expect(1).to.equal(1);
   });
 });
+
+// hint, you can only test what's exported
+describe('template.js', function() {
+  it('contains article', function() {
+    expect(typeof article.innerHTML).to.equal('string');
+  });
+});
